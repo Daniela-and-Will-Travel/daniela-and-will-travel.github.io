47b2a8838224ab5ee848d74498fee4b57f3ba26f
William Vandergraaf <willtheorangeguy@users.noreply.github.com>
2024-08-07 13:19:40 -0600

    Update config

--- src/_data/translations.js
+++ src/_data/translations.js
@@ -1,11 +1,11 @@
 module.exports = {
     en: {
         meta: {
-            title: 'elva',
+            title: 'Daniela and Will Travel',
             rssTitle: 'RSS Feed',
             jsonTitle: 'JSON Feed',
-            description: 'A multilingual, clean, green, 11ty starter theme',
-            opengraphDefaultAlt: 'An English description of the default opengraph image'
+            description: "Daniela and Will's travel blog featuring travel guides and tips and tricks for a gap year throughout Europe.",
+            opengraphDefaultAlt: 'An English description of the default opengraph image.'
         },
         header: {
             skipLink: 'Skip to content',
@@ -18,33 +18,33 @@ module.exports = {
         },
         feeds: {
             info: 'This is an RSS feed. Copy and paste the URL into your feed reader. Visit <a href="https://aboutfeeds.com">About Feeds</a> to learn more about RSS.',
-            title: 'Recently published'
+            title: 'Recently Published'
         },
         dark: 'Dark',
         light: 'Light'
     },
     es: {
         meta: {
-            title: 'elva',
+            title: 'Daniela y Will Travel',
             rssTitle: 'RSS Feed',
             jsonTitle: 'JSON Feed',
-            description: 'A multilingual clean green 11ty starter theme',
-            opengraphDefaultAlt: 'A Swedish description of the default opengraph image'
+            description: 'El blog de viajes de Daniela y Will con guías de viaje y consejos para un año sabático por Europa.',
+            opengraphDefaultAlt: 'Una descripción en español de la imagen predeterminada de opengraph.'
         },
         header: {
-            skipLink: 'Hoppa till innehållet',
-            home: 'Hem'
+            skipLink: 'Saltar al contenido',
+            home: 'Inicio'
         },
         readingTime: {
-            underMinute: 'Mindre än 1 minut att läsa',
-            minute: '1 minut till läsaren',
-            other: '{{ minutes }} minuter att läsa'
+            underMinute: 'Menos de 1 minuto para leer',
+            minute: '1 minuto para leer',
+            other: '{{ minutes }} minutos para leer'
         },
         feeds: {
-            info: 'Detta är ett RSS feed. Kopiera och klistra in webbadressen i din flödesläsare. Besök <a href="https://aboutfeeds.com">About Feeds</a> för att lära dig mer om RSS.',
-            title: 'Nyligen publicerad'
+            info: 'Este es un feed RSS. Copia y pega la URL en tu lector de feeds. Visita <a href="https://aboutfeeds.com">About Feeds</a> para obtener más información sobre RSS.',
+            title: 'Publicado Recientemente'
         },
-        dark: 'Mörk',
-        light: 'Ljus'
+        dark: 'Oscuro',
+        light: 'Claro'
     }
 }
