20c5f9b187125f15c42b07e7fad480627a866cc7
William Vandergraaf <willtheorangeguy@users.noreply.github.com>
2024-08-08 09:43:45 -0600

    Try to get the main page working

---
+++ styles/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: ['_site/**/*.html'],
+    safelist: [],
+    theme: {
+      extend: {
+        colors: {
+          change: 'transparent',
+        },
+      },
+    },
+    plugins: [],
+  }
+  
