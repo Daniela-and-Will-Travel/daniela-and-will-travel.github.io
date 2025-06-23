b254fd99e5391226c11e5a5fc7209e4d224c2a13
William Vandergraaf <willtheorangeguy@users.noreply.github.com>
2025-06-23 11:46:51 -0700

    build: try new build process

--- build-win.bat
+++ build-win.bat
@@ -3,19 +3,5 @@
 echo Running npm command...
 npx @11ty/eleventy
 
-:: Check if the npm command was successful
-if %errorlevel% neq 0 (
-    echo npm command failed. Exiting script.
-    exit /b %errorlevel%
-)
-
-:: Delete all files in the docs folder
-echo Deleting all files in the docs folder...
-del /q /f docs\*
-
-:: Move all files from dist folder to docs folder
-echo Moving files from dist folder to docs folder...
-move /y dist\* docs\
-
 echo Script completed successfully.
 pause
