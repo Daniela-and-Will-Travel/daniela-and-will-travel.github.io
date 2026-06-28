709313753ef1f0f52a9668adae0321341e4ad944
willtheorangeguy <willtheorangeguy@users.noreply.github.com>
2026-06-27 21:50:14 -0600

    ci: update build workflow

--- .eleventy.js
+++ .eleventy.js
@@ -8,7 +8,7 @@ const markdownItIns = require('markdown-it-ins');
 const markdownItMark = require('markdown-it-mark');
 const markdownItSub = require('markdown-it-sub');
 const markdownItSup = require('markdown-it-sup');
-const pluginRSS = require('@11ty/eleventy-plugin-rss');
+const pluginRSS = require('@11ty/eleventy-plugin-rss').default || require('@11ty/eleventy-plugin-rss');
 const pluginSyntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 const pluginEmbedEverything = require('eleventy-plugin-embed-everything');
 
